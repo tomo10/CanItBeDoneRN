3275f744171f963a925a66cdc403258013cc19b1
Thomas Edwards <thce28@gmail.com>
2020-02-24 16:58:38 +0000

    timing bubbles complet

--- src/Timing/Timings.tsx
+++ src/Timing/Timings.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
-import { loop } from "react-native-redash";
 import { useMemoOne } from "use-memo-one";
 
 import SimpleActivityIndicator from "./SimpleActivityIndicator";
 import { Button, StyleGuide } from "../components";
-import { State } from "react-native-gesture-handler";
 
 const {
   Clock,
@@ -20,7 +18,7 @@ const {
   clockRunning,
   and,
   not,
-  eq, 
+  eq,
   timing
 } = Animated;
 
@@ -32,42 +30,62 @@ const styles = StyleSheet.create({
   }
 });
 
-const runTiming = (clock: Animated.Clock) => {
-    const state = {
-        finished: new Value(0),
-        position: new Value(0),
-        frameTime: new Value(0),
-        time: new Value(0)
-    };
-    const config = {
-        toValue: new Value(1),
-        duration: 1500,
-        easing: Easing.linear
-    }
-    
-    return block([
-        cond(not(clockRunning(clock)), startClock(clock)), // if clock not running, then we start clock
-        timing(clock, state, config),
-        cond(eq(state.finished, 1), [
-            set(state.finished, 0),
-            set(state.position, 0),
-            set(state.frameTime, 0),
-            set(state.time, 0),
-        ]),
-        state.position
-    ])
-}
+const runTiming = (clock: Animated.Clock): Animated.Node<number> => {
+  const state: Animated.TimingState = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+  const config = {
+    toValue: new Value(1),
+    duration: 1000,
+    easing: Easing.linear
+  };
+  return block([
+    cond(
+      not(clockRunning(clock)),
+      set(state.time, 0),
+      timing(clock, state, config)
+    ),
+    cond(eq(state.finished, 1), [
+      set(state.finished, 0),
+      set(state.frameTime, 0),
+      set(state.time, 0),
+      set(config.toValue, cond(eq(state.position, 1), 0, 1))
+    ]),
+    state.position
+  ]);
+};
 
 export default () => {
-  const progress = new Value(0);
-  const clock = new Clock();
-  useCode(block([
-    set(progress, runTiming(clock))
-  ]), []);
-
+  const [play, setPlay] = useState(true);
+  const { clock, isPlaying, progress } = useMemoOne(
+    () => ({
+      clock: new Clock(),
+      isPlaying: new Value(0) as Animated.Value<0 | 1>,
+      progress: new Value(0)
+    }),
+    []
+  );
+  isPlaying.setValue(play ? 1 : 0);
+  useCode(
+    () =>
+      block([
+        cond(and(isPlaying, not(clockRunning(clock))), startClock(clock)),
+        cond(and(not(isPlaying), clockRunning(clock)), stopClock(clock)),
+        set(progress, runTiming(clock))
+      ]),
+    [clock, isPlaying, progress]
+  );
   return (
     <View style={styles.container}>
-      <SimpleActivityIndicator {...{progress}} />
+      <SimpleActivityIndicator {...{ progress }} />
+      <Button
+        label={play ? "Pause" : "Play"}
+        primary
+        onPress={() => setPlay(prev => !prev)}
+      />
     </View>
   );
 };
