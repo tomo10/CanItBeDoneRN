0e4683099ffe71be82ed513c72860307101e38d3
Thomas Edwards <thce28@gmail.com>
2020-02-26 13:02:02 +0000

    svg example done with pot minor bug

--- src/Svg/Svg.tsx
+++ src/Svg/Svg.tsx
@@ -3,12 +3,12 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import Svg, { Ellipse } from "react-native-svg";
 import Constants from "expo-constants";
 
-import { StyleGuide } from "../components";
+import { StyleGuide, withTransition } from "../components";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
 import { onGestureEvent } from "react-native-redash";
 
-const { Value, sub, multiply } = Animated;
+const { Value, sub, multiply, eq, cond, max, abs } = Animated;
 
 const { width, height } = Dimensions.get("window");
 const AnimatedEllipse = Animated.createAnimatedComponent(Ellipse);
@@ -25,9 +25,12 @@ const styles = StyleSheet.create({
   }
 });
 
-const canvas2Euclidean = (x: Animated.Value<number>, y: Animated.Value<number>) => {
+const canvas2Euclidean = (
+    x: Animated.Node<number>, 
+    y: Animated.Node<number>
+  ) => {
   return {
-    rx: sub(x, center.y),
+    rx: sub(x, center.x),
     ry: multiply(sub(y, center.y), -1)
   }
 }
@@ -35,21 +38,31 @@ const canvas2Euclidean = (x: Animated.Value<number>, y: Animated.Value<number>) 
 export default () => {
     const x = new Value(0);
     const y = new Value(0);
+    const velocityX = new Value(0);
+    const velocityY = new Value(0);
     const state = new Value(State.UNDETERMINED);
     const gestureHandler = onGestureEvent({
         x,
         y,
+        velocityX,
+        velocityY,
         state
     });
-    const {rx, ry} = canvas2Euclidean(x, y);
+    const isActive = eq(state, State.ACTIVE);
+    const targetX = cond(isActive, x, center.x)
+    const targetY = cond(isActive, y, center.y)
+    const {rx, ry} = canvas2Euclidean(
+      withTransition(targetX, velocityX),
+      withTransition(targetY, velocityY)
+      );
   return (
     <View style={styles.container}>
       <Svg style={StyleSheet.absoluteFill}>
         <AnimatedEllipse
           cx={center.x}
           cy={center.y}
-          rx={rx}
-          ry={ry}
+          rx={max(abs(rx), radius)}
+          ry={max(abs(ry), radius)}
           fill={StyleGuide.palette.primary}
         />
       </Svg>
