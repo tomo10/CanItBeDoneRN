2259afc4c21d1b4112202ecceca6db3700c9e5c6
Thomas Edwards <thce28@gmail.com>
2020-02-26 14:08:43 +0000

    circles rotate wrong way

--- src/Trigonometry/CircularProgress.tsx
+++ src/Trigonometry/CircularProgress.tsx
@@ -3,13 +3,21 @@ import { StyleSheet, View } from "react-native";
 import Svg, { Circle } from "react-native-svg";
 import Color from "color";
 import { Feather as Icon } from "@expo/vector-icons";
+import Animated from 'react-native-reanimated';
+import { bInterpolate } from "react-native-redash";
+
+const { multiply } = Animated;
 
 export const STROKE_WIDTH = 40;
+const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  svg: {
+    transform: [{ rotate: "-90deg"}]
   }
 });
 
@@ -17,22 +25,28 @@ interface CircularProgressProps {
   icon: string;
   color: string;
   size: number;
-  progress: number;
+  progress: Animated.Node<number>;
 }
 
 export default ({ color, size, progress, icon }: CircularProgressProps) => {
   const r = (size - STROKE_WIDTH) / 2;
   const cx = size / 2;
   const cy = size / 2;
+  const circumferencce = r * 2 * Math.PI;
+  const a = bInterpolate(progress, 0, Math.PI * 2);
+  const strokeDashoffset = multiply(a, r);
+
   return (
     <View style={styles.container}>
-      <Svg width={size} height={size}>
-        <Circle
+      <Svg style={styles.svg} width={size} height={size}>
+        <AnimatedCircle
           stroke={color}
           fill="none"
           strokeLinecap="round"
+          strokeDasharray={`${circumferencce}, ${circumferencce}`}
           strokeWidth={STROKE_WIDTH}
           {...{
+            strokeDashoffset,
             cx,
             cy,
             r
--- src/Trigonometry/Trigonometry.tsx
+++ src/Trigonometry/Trigonometry.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
-import { timing } from "react-native-redash";
+import { timing, bInterpolate } from "react-native-redash";
 
 import Animated from 'react-native-reanimated';
 import CircularProgress, { STROKE_WIDTH } from "./CircularProgress";
 
-const { Value } = Animated;
+const { Value, useCode, set } = Animated;
 const { width } = Dimensions.get("window");
 const s1 = width - 64;
 const s2 = s1 - STROKE_WIDTH * 2;
@@ -21,25 +21,28 @@ const styles = StyleSheet.create({
 
 export default () => {
     const progress = new Value(0);
-
+    const p1 = bInterpolate(progress, 0, 0.33); // bInterpolate means binary interpolate. means if going from 0 to 1 (or 0.33 here) dont need write input range 0 to 1 like you do with interpolate
+    const p2 = bInterpolate(progress, 0, 0.5);
+    const p3 = bInterpolate(progress, 0, 0.8);
+    useCode(() => set(progress, timing({ duration: 5000 })), [])
   return (
     <View style={styles.container}>
       <CircularProgress
         size={s1}
         color="#DF0B18"
-        progress={0.33}
+        progress={p1}
         icon="chevron-right"
       />
       <CircularProgress
         size={s2}
         color="#48E101"
-        progress={0.5}
+        progress={p2}
         icon="chevrons-right"
       />
       <CircularProgress
         size={s3}
         color="#00C3DD"
-        progress={0.8}
+        progress={p3}
         icon="chevron-up"
       />
     </View>
