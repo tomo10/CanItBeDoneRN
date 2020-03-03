d420c6803a6a0eb8f63af07951129e120ba5e0ca
Thomas Edwards <thce28@gmail.com>
2020-03-03 12:19:52 +0000

    slide fan tiles work

--- src/Transitions/EventTransition.tsx
+++ src/Transitions/EventTransition.tsx
@@ -7,40 +7,33 @@ import {
   bInterpolate,
   withTransition
 } from "react-native-redash";
-import { State, TapGestureHandler } from "react-native-gesture-handler";
+import { State, TapGestureHandler, LongPressGestureHandler } from "react-native-gesture-handler";
 
 import { tiles, Tile, Button, StyleGuide } from "../components";
 
-const { multiply, interpolate, not, cond, eq, add, useCode, set, Value, } = Animated;
+const { multiply, block, not, cond, eq, add, useCode, set, Value, } = Animated;
 const { width } = Dimensions.get("window");
 
 
-const withOffset = (
+const withFan = (
+  open: Animated.Value<number>,
   value: Animated.Value<number>,
   state: Animated.Value<number>,
-  offset: Animated.Value<number>
+  // offset: Animated.Value<number>
   ) => {
       return cond(
-          eq(state, State.END), 
-          [set(offset, add(offset, value)), offset] // set offset to current value and return offset
-          
+          eq(state, State.ACTIVE), 
+          set(open, not(open)) 
       )
   }
 
 
 export default () => {
-    // const [toggled, setToggled] = useState<0 | 1>(0);
 
-    // const transition = useTransition(toggled, not(toggled), toggled);
-    // const transition = (
-    //   <Transition.Change durationMs={500} interpolation="easeInOut"  />
-    //   );
     const open = new Value<0 | 1>(0);
-
-    const transition = withTransition(open);
-
     const state = new Value(State.UNDETERMINED);
     const translationY = new Value(0);
+    const transition = withTransition(open);
 
     const gestureHandler = onGestureEvent(
         {
@@ -49,35 +42,32 @@ export default () => {
         }
     );
 
-  //   const height = bInterpolate(
-  //     transition,
-  //     0,
-  //     240
-  // );
-
     const transY = bInterpolate(transition, 0, 80);
 
-    // const addY = add(offsetY, translationX);
-    // const addX = add(offsetX, translationY);
+    // useCode(() => cond(eq(state, State.END), set(open, not(open))), [
+    //   open, 
+    //   state
+    // ]);
 
-    // const translateX = withOffset(addX, state, offsetX);
-    // const translateY = cond(eq(state, State.ACTIVE), addY, set(offsetY, addY));
-    // const translateX = cond(eq(state, State.ACTIVE), addY, set(offsetY, addY));
-
-    useCode(() => cond(eq(state, State.END), set(open, not(open))), [
-      open, state
-    ]);
+    
+    // this block fans tiles when held > 0.5s and then released. 
+    useCode(() => block([
+      cond(eq(state, State.BEGAN), set(open, 1)),
+      cond(eq(state, State.END), set(open, 0))
+    ]), [
+      open,
+      state
+    ])
 
     return (
         <View style={styles.container}>
-            {tiles.map((tile, index) => {
+            {/* {tiles.map((tile, index) => {
                 let direction = 0;
                 if (index === 0) direction = -1;
                 else if (index === 2) direction = 1;
 
-                const newb = multiply(direction, transY);
+                const fanY = multiply(direction, transY);
                 
-
                 return (
                   <TapGestureHandler {...gestureHandler}>
                       <Animated.View
@@ -85,12 +75,36 @@ export default () => {
                         style={[
                           styles.overlay,
                           {
-                          transform: [{ translateY: newb }]
+                          transform: [{ translateY: fanY }]
                           }]}
                       >
                         <Tile {...{ tile }} />            
                       </Animated.View>
                   </TapGestureHandler>
+  
+                )
+            })} */}
+            {tiles.map((tile, index) => {
+                let direction = 0;
+                if (index === 0) direction = -1;
+                else if (index === 2) direction = 1;
+
+                const fanY = multiply(direction, transY);
+                
+                return (
+                  <TapGestureHandler {...gestureHandler}>
+                      <Animated.View
+                        key={tile.id}
+                        style={[
+                          styles.overlay,
+                          {
+                          transform: [{ translateY: fanY }]
+                          }]}
+                      >
+                        <Tile {...{ tile }} />            
+                      </Animated.View>
+                  </TapGestureHandler>
+  
                 )
             })}
             {/* <Button label={toggled ? "Reset" : "Start"} primary onPress={() => setToggled(toggled ^ 1)} /> */}
