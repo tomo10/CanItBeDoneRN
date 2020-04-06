3cdfc9aa794862463c565f6bea838073fcff0630
Thomas Edwards <thce28@gmail.com>
2020-04-06 12:11:56 +0100

    swipe working

--- src/Swipe/Swipeable.tsx
+++ src/Swipe/Swipeable.tsx
@@ -1,169 +1,73 @@
 import * as React from "react";
-import { PanGestureHandler, State } from "react-native-gesture-handler";
-import Animated from "react-native-reanimated";
-import { StyleSheet, ARTText} from "react-native";
+import { StyleSheet } from "react-native";
 import { onGestureEvent } from "react-native-redash";
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { withSpring } from "../components";
 
-
-const {
-  Clock,
-  Value,
-  block,
-  useCode,
-  cond,
-  not,
-  clockRunning,
-  startClock,
-  timing: reTiming,
-  spring: reSpring,
-  stopClock,
-  add,
-  multiply,
-  call,
-  and,
-  eq,
-  abs,
-  sub,
-  neq,
-  min,
-  set,
-  defined
-} = Animated;
-
-
-
-interface WithSpringParams {
-  value: Animated.Adaptable<number>;
-  velocity: Animated.Adaptable<number>;
-  state: Animated.Value<State>;
-  snapPoints: Animated.Adaptable<number>[];
-  offset?: Animated.Value<number>;
-  config?: SpringConfig;
-  onSnap?: (value: readonly number[]) => void;
-}
-
-const snapPoint = (
-  value: Animated.Adaptable<number>,
-  velocity: Animated.Adaptable<number>,
-  points: Animated.Adaptable<number>[]
-) => {
-  const point = add(value, multiply(0.2, velocity));
-  const diffPoint = (p: Animated.Adaptable<number>) => abs(sub(point, p));
-  const deltas = points.map(p => diffPoint(p));
-  const minDelta = min(...deltas);
-  return points.reduce(
-    (acc, p) => cond(eq(diffPoint(p), minDelta), p, acc),
-    new Value()
-  );
+const { Value, useCode, block, set } = Animated;
+const config = {
+  damping: 40,
+  mass: 1,
+  stiffness: 300,
+  overshootClamping: false,
+  restSpeedThreshold: 1,
+  restDisplacementThreshold: 1
 };
-
-const withSpring = (props: WithSpringParams) => {
-  const {
-    value,
-    velocity,
-    state,
-    snapPoints,
-    offset,
-    config: springConfig,
-    onSnap
-  } = {
-    offset: new Value(0),
-    ...props
-  };
-  const clock = new Clock();
-  const springState: Animated.SpringState = {
-    finished: new Value(0),
-    velocity: new Value(0),
-    position: new Value(0),
-    time: new Value(0)
-  };
-
-  const config: PrivateSpringConfig = {
-    toValue: new Value(0),
-    damping: 15,
-    mass: 1,
-    stiffness: 150,
-    overshootClamping: false,
-    restSpeedThreshold: 1,
-    restDisplacementThreshold: 1,
-    ...springConfig
-  };
-
-  const gestureAndAnimationIsOver = new Value(1);
-  const isSpringInterrupted = and(eq(state, State.BEGAN), clockRunning(clock));
-  const finishSpring = [
-    set(offset, springState.position),
-    stopClock(clock),
-    set(gestureAndAnimationIsOver, 1)
-  ];
-  const snap = onSnap
-    ? [cond(clockRunning(clock), call([springState.position], onSnap))]
-    : [];
-  return block([
-    cond(isSpringInterrupted, finishSpring),
-    cond(gestureAndAnimationIsOver, set(springState.position, offset)),
-    cond(neq(state, State.END), [
-      set(gestureAndAnimationIsOver, 0),
-      set(springState.finished, 0),
-      set(springState.position, add(offset, value))
-    ]),
-    cond(and(eq(state, State.END), not(gestureAndAnimationIsOver)), [
-      cond(and(not(clockRunning(clock)), not(springState.finished)), [
-        set(springState.velocity, velocity),
-        set(springState.time, 0),
-        set(
-          config.toValue,
-          snapPoint(springState.position, velocity, snapPoints)
-        ),
-        startClock(clock)
-      ]),
-      reSpring(clock, springState, config),
-      cond(springState.finished, [...snap, ...finishSpring])
-    ]),
-    springState.position
-  ]);
-};
-
-
 interface SwipeableProps {
-  translateX: Animated.Value<number>;
-  translateY: Animated.Value<number>;
+  x: Animated.Value<number>;
+  y: Animated.Value<number>;
+  offsetX?: Animated.Value<number>;
+  offsetY?: Animated.Value<number>;
   snapPoints: number[];
+  onSnap: (value: readonly number[]) => void;
 }
 
-
-export default ({ translateX, translateY }: SwipeableProps) => {
+export default ({
+  x,
+  y,
+  offsetX,
+  offsetY,
+  snapPoints,
+  onSnap
+}: SwipeableProps) => {
   const translationX = new Value(0);
   const translationY = new Value(0);
   const velocityX = new Value(0);
   const velocityY = new Value(0);
   const state = new Value(State.UNDETERMINED);
   const gestureHandler = onGestureEvent({
+    velocityX,
     translationX,
     translationY,
-    velocityX, 
-    velocityY, 
     state
-  })
-
-  const x = withSpring({
+  });
+  const translateX = withSpring({
     value: translationX,
     velocity: velocityX,
+    offset: offsetX,
     state,
-    snapPoints: [0]
+    snapPoints,
+    onSnap,
+    config
   });
-  const y = withSpring({
+  const translateY = withSpring({
     value: translationY,
     velocity: velocityY,
+    offset: offsetY || new Value(0),
     state,
-    snapPoints: [0]
+    snapPoints: [0],
+    config
   });
-
-  useCode(block([set(translateX, x), set(translateY, y)]), [])
-
+  useCode(() => block([set(x, translateX), set(y, translateY)]), [
+    translateX,
+    translateY,
+    x,
+    y
+  ]);
   return (
-      <PanGestureHandler {...gestureHandler}>
-        <Animated.View style={StyleSheet.absoluteFill} />
-      </PanGestureHandler>
+    <PanGestureHandler {...gestureHandler}>
+      <Animated.View style={StyleSheet.absoluteFill} />
+    </PanGestureHandler>
   );
 };
